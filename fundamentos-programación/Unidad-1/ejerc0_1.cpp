67a05db193cf2d58cb3e34f4d3a64d4bde98baaa
Angel Pusch <contact@leznom.com.ar>
2022-06-27 14:38:10 -0300

    🥲️

--- fundamentos-programación/Unidad-1/ejerc0_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int FA=2022,FdN=0,Ed=0;
-    cout<<"Fecha de nacimiento: ";
-    cin>>FdN;
-    Ed=FA-FdN;
-    cout<<"Edad: "<<Ed<<endl;
-}
