0146a5fe429b809863993b0aba446a02aee634dd
Angel Pusch <contact@leznom.com.ar>
2022-06-28 16:19:27 -0300

    Acomodado 😌️

--- fundamentos-programación/guía 1/ejerc1_4.cpp
+++ fundamentos-programación/guía 1/ejerc1_4.cpp
@@ -1,26 +1,38 @@
 #include<iostream>
+using std::cout,std::cin,std::endl;
 
-int main(){
-    const float boleto_estudiantil=17.00;
-    float total_recaudado=0,boleto=0.00,boleto_trabajador=0.00;
-    int boletos_vendidos=0,trabajadores=0,estudiantes=0;
+int main()
+{
+  const float boleto_estudiantil = 17.0;
+  float total_recaudado = 0.0, boleto = 0.0, boleto_trabajador = 0.0;
+  int boletos_vendidos = 0, trabajadores = 0, estudiantes = 0;
 
-    std::cout<<"Total de boletos vendidos: ";
-    std::cin>>boletos_vendidos;
+  cout << "Total de boletos vendidos: ";
+  cin >> boletos_vendidos;
 
-    std::cout<<"Cantidad de Trabajadores: ";
-    std::cin>>trabajadores;
+  cout << "Cantidad de Trabajadores: ";
+  cin >> trabajadores;
 
-    std::cout<<"Cantidad de Estudiantes: ";
-    std::cin>>estudiantes;
+  cout << "Cantidad de Estudiantes: ";
+  cin >> estudiantes;
 
-    boleto=boleto_estudiantil*100/50;
-    boleto_trabajador=boleto-(boleto*40/100);
+  boleto = boleto_estudiantil * 100.0 / 50.0;
+  boleto_trabajador = boleto - (boleto * 40.0 / 100.0);
 
-    if(estudiantes==0 || trabajadores==0)
-            total_recaudado=boletos_vendidos*boleto;
-    if(estudiantes!=0 || trabajadores!=0)
-            total_recaudado=(boletos_vendidos*boleto)-(trabajadores*boleto)+(trabajadores*boleto_trabajador)-(estudiantes*boleto)+(estudiantes*boleto_estudiantil);
-    std::cout<<"El total recaudado por los boletos es la suma de: $"<<total_recaudado<<std::endl;
-    return 0;
+  if(estudiantes == 0 || trabajadores == 0)
+      {
+          total_recaudado = boletos_vendidos * boleto;
+      }
+  else if(estudiantes != 0 || trabajadores != 0)
+      {
+          total_recaudado= (boletos_vendidos * boleto) -
+                           (trabajadores * boleto ) +
+                           (trabajadores * boleto_trabajador) -
+                           (estudiantes * boleto) +
+                           (estudiantes * boleto_estudiantil);
+      }
+  cout << "El total recaudado por los boletos es la suma de: $"
+       << total_recaudado
+       << endl;
+  return 0;
 }
