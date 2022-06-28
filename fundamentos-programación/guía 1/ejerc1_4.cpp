5d7ddf6836e11feda7a6a211d8644aabb979baf8
Angel Pusch <contact@leznom.com.ar>
2022-06-28 15:03:35 -0300

    Ahora funciona 🥳️

--- fundamentos-programación/guía 1/ejerc1_4.cpp
+++ fundamentos-programación/guía 1/ejerc1_4.cpp
@@ -15,11 +15,12 @@ int main(){
     std::cin>>estudiantes;
 
     boleto=boleto_estudiantil*100/50;
-    boleto_trabajador=boleto*40/100;
+    boleto_trabajador=boleto-(boleto*40/100);
+
     if(estudiantes==0 || trabajadores==0)
             total_recaudado=boletos_vendidos*boleto;
     if(estudiantes!=0 || trabajadores!=0)
-            total_recaudado=(boletos_vendidos*boleto)-(trabajadores*boleto_trabajador)-(estudiantes*boleto_estudiantil);
-    std::cout<<"El total recaudado es la suma de: $"<<total_recaudado<<std::endl;
+            total_recaudado=(boletos_vendidos*boleto)-(trabajadores*boleto)+(trabajadores*boleto_trabajador)-(estudiantes*boleto)+(estudiantes*boleto_estudiantil);
+    std::cout<<"El total recaudado por los boletos es la suma de: $"<<total_recaudado<<std::endl;
     return 0;
 }
