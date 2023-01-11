687ec7d841ad180d89c3f1cc2a0bad6d28d6221f
Angel Pusch <eangel00302@gmail.com>
2023-01-11 12:09:16 -0300

    correcíon actividad 6

--- guide4_act6.cpp
+++ guide4_act6.cpp
@@ -9,19 +9,17 @@ using std::string;
 int main()
 {
     int palabras_num = 0;
-    int contador = 0;
     int i = 0;
     string palabra;
     cout << "Inserte un texto: ";
     getline(cin, palabra);
 
-    int palabras_num = 0, contador = 0, i = 0;
+    int contador = palabra.size() - 1;
     char primer_letra = palabra[0];
-    contador = palabra.size() - 1;
     char ultima_letra = palabra[contador];
 
-    cout << primer_letra << endl;
-    cout << ultima_letra << endl;
+    cout << "El texto comienza con la letra: " << primer_letra << endl;
+    cout << "Y finaliza con la letra: " << ultima_letra << endl;
 
     while (i != contador)
     {
@@ -34,6 +32,6 @@ int main()
         i++;
     }
 
-    cout << "La cantidad de palabras es de: " << palabras_num+1 << endl;
+    cout << "La cantidad de palabras es de: " << palabras_num + 1 << endl;
     return EXIT_SUCCESS;
 }
